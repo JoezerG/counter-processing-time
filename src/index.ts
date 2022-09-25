a984bd2d96007d833b3ad629388610f3e08d7e44
JoezerG <joezergullo@gmail.com>
2022-09-25 15:16:58 -0500

    counte module

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+import { hrtime } from "process";
+
+export enum TimeUnit {
+  SECONDS,
+  MILLISECONDS,
+}
+
+interface ProcessingTimeCalculatorProps {
+  message?: string;
+  timeUnit?: TimeUnit;
+}
+
+export default class ProcessingTimeCalculator {
+  // private readonly logger = new Logger(ProcessingTimeCalculator.name);
+  private startCounter: [number, number];
+  private message: string = "";
+  private timeUnit: TimeUnit = TimeUnit.MILLISECONDS;
+
+  constructor(props?: ProcessingTimeCalculatorProps) {
+    this.startCounter = hrtime();
+    if (props) {
+      if (props.message) {
+        this.message = props.message;
+      }
+
+      if (props.timeUnit) {
+        this.timeUnit = props.timeUnit;
+      }
+    }
+  }
+
+  stop() {
+    const _stop = hrtime(this.startCounter);
+    const elapsedTimeMili = Math.floor((_stop[0] * 1e6 + _stop[1]) / 1e6); // miliseconds
+    const elapsedTimeSeconds = (_stop[0] * 1e9 + _stop[1]) / 1e9; // seconds
+    if (this.message !== "") {
+      switch (this.timeUnit) {
+        case TimeUnit.MILLISECONDS:
+          console.log(`${this.message} | ${elapsedTimeMili} milliseconds`);
+          break;
+
+        case TimeUnit.SECONDS:
+          console.log(`${this.message} | ${elapsedTimeSeconds} seconds`);
+          break;
+      }
+    }
+
+    switch (this.timeUnit) {
+      case TimeUnit.MILLISECONDS:
+        return elapsedTimeMili;
+
+      case TimeUnit.SECONDS:
+        return elapsedTimeSeconds;
+    }
+  }
+}
